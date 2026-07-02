947a487c77dd02109e1bc945891b63015af68e20
Alwin Jose George <alwinjosegeorge2028@cs.sjcetpalai.ac.in>
2026-07-02 06:51:23 +0530

    feat: update desktop background video for FORMULATED WITH INGREDIENTS section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -320,9 +320,9 @@ function Home() {
           allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
           style={{ border: "none" }}
         />
-        {/* Desktop/Laptop Background Video (Untitled design) */}
+        {/* Desktop/Laptop Background Video (Untitled design (1) (1)) */}
         <iframe
-          src="https://player.cloudinary.com/embed/?cloud_name=dar4m0jyt&public_id=Untitled_design_wwnjyc&autoplay=true&loop=true&muted=true&controls=false&player[autoplay]=true&player[loop]=true&player[muted]=true&player[controls]=false"
+          src="https://player.cloudinary.com/embed/?cloud_name=dar4m0jyt&public_id=Untitled_design_1_1_mxi7vn&autoplay=true&loop=true&muted=true&controls=false&player[autoplay]=true&player[loop]=true&player[muted]=true&player[controls]=false"
           className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full z-0 pointer-events-none border-none scale-[2.4] origin-top hidden md:block"
           allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
           style={{ border: "none" }}
